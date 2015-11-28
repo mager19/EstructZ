5015e8d928d0eb0a7ff8d1324ade52688156ad85
mager19 <mager19@gmail.com>
2015-11-28 09:39:46 -0500

    añadido el lightbox

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
     
 
+$(".fancybox").fancybox({
+        'width'             : '75%',
+             'height'            : '75%',
+             'autoScale'         : false,
+             'transitionIn'      : 'none',
+             'transitionOut'     : 'none',
+             'type'              : 'iframe',
+             'showCloseButton'   : true,
+             'opacity'   : true
+    }).trigger("click")
 
 $('.owl-carousel').owlCarousel({
     loop:true,
@@ -43,18 +53,6 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
 
 
 });
